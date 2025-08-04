5b733791684ecf0981a27c6ccdb5c7f843e8b0bd
Joshua Tag Howard <tag@jthoward.dev>
2025-08-04 01:36:42 -0400

    Add link creation and validation between SOURCE_DIR and TARGET_DIR

--- env.ts
+++ env.ts
@@ -1,4 +1,4 @@
-import { stat } from "node:fs/promises";
+import { link, stat, unlink } from "node:fs/promises";
 
 const inDirRaw = process.env.SOURCE_DIR;
 const outDirRaw = process.env.TARGET_DIR;
@@ -20,6 +20,33 @@ if (!outDirStat || !outDirStat.isDirectory()) {
   console.error(
     `TARGET_DIR "${outDirRaw}" is not a directory or does not exist.`
   );
+  process.exit(1);
+}
+
+// Make a test link across the directories to ensure they are valid
+const testFile = `${inDirRaw}/test-link`;
+const targetFile = `${outDirRaw}/test-link`;
+try {
+  await Bun.write(testFile, "test");
+  await link(testFile, targetFile);
+  await unlink(testFile);
+  await unlink(targetFile);
+} catch (error) {
+  console.error(
+    `Failed to create a test link between SOURCE_DIR and TARGET_DIR. Ensure that you mounted a single docker volume and that both SOURCE_DIR and TARGET_DIR are subdirectories of that volume. Error: ${String(
+      error
+    )}`
+  );
+  try {
+    await unlink(testFile);
+  } catch {
+    // Ignore if the test link was not created
+  }
+  try {
+    await unlink(targetFile);
+  } catch {
+    // Ignore if the test link was not created
+  }
   process.exit(1);
 }
 
